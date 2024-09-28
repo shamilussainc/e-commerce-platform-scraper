a6c97d1b560fec608046acca58b341b1349ccae6
Shamil Ussain C <shamilussain@gmail.com>
2024-09-28 17:58:30 +0530

    feat: scrap multiple pages.

--- worker/app/web_crawler.py
+++ worker/app/web_crawler.py
@@ -5,7 +5,7 @@ from app.utils import send_product_to_server
 class ProductsSpider(scrapy.Spider):
     name = "products"
     start_urls = [
-        os.getenv("SCRAPY_START_URL"),
+        os.getenv("SCRAPY_START_URL")+"?currentPage=1",
     ]
 
     def parse(self, response):            
@@ -14,6 +14,15 @@ class ProductsSpider(scrapy.Spider):
         for link in links:
             product_link = response.urljoin(link)
             yield response.follow(product_link, self.parse_product)
+
+        # Get the current page number from the URL
+        current_page = int(response.url.split('currentPage=')[-1])
+        next_page = current_page + 1
+        # Construct the next page URL
+        next_page_url = response.urljoin(f"{os.getenv('SCRAPY_START_URL')}?currentPage={next_page}")
+        # limiting number of pages for scrapping.
+        if next_page <= 10:
+            yield scrapy.Request(next_page_url, callback=self.parse)
 
 
     def parse_product(self, response):
@@ -24,3 +33,5 @@ class ProductsSpider(scrapy.Spider):
             "product_condition": ''.join(response.css('td.RightItemDetail > div.itemState > p.state > span::text').getall()),
         }
         send_product_to_server(product)
+
+
